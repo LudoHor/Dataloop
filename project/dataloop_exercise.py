98d3286643710e218fabc6e987e6ab5796344208
Ludovit Horvath <ludovit.horvath.94@gmail.com>
2023-08-30 12:56:58 +0200

    err 2

--- project/dataloop_exercise.py
+++ project/dataloop_exercise.py
@@ -10,13 +10,8 @@ class Data:
                     raise ValueError("size should be either None or of type number.")
                 if height is not None and not isinstance(height, (int, float)):
                     raise ValueError("height should be either None or of type int.")
-                self.size = None
-                self._height = None
-                # size and heihgt to float
-                if size is not None:
-                    self.size = float(size)
-                if height is not None:
-                    self._height = float(height)
+                self.size = float(size) if size is not None else None
+                self._height = float(height) if height is not None else None
 
             @property
             def height(self) -> int:
@@ -48,9 +43,7 @@ class Data:
             raise ValueError("id should be either None or of type str or int.")
         if name is not None and not isinstance(name, str):
             raise ValueError("name should be either None or of type str.")
-        self.id = None
-        if id is not None:
-            self.id = str(id)
+        self.id = str(id) if id is not None else None
         self.name = name
         self.metadata: 'Data.Metadata'  # Type hint for metadata
 
